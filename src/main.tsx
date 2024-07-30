06b2e5d83c90f0aea75c0b3e5e33332c59c93a47
Nonthaphat Chaisu <48936015+Podter@users.noreply.github.com>
2024-07-30 22:50:55 +0700

    removed boilerplate

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-import App from "./App.tsx";
-
-import "./index.css";
+import "./styles.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
+    <h1>Hello, world!</h1>
   </React.StrictMode>,
 );
