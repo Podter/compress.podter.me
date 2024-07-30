a0d52c3790358a77eaa27253fc54549fd1f64871
Nonthaphat Chaisu <48936015+Podter@users.noreply.github.com>
2024-07-30 23:22:24 +0700

    Update header.tsx

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -2,7 +2,7 @@ import ThemeSwitch from "./theme-switch";
 
 export default function Header() {
   return (
-    <header className="fixed left-0 top-0 z-50 flex h-12 w-full place-items-center justify-end space-x-1 px-2">
+    <header className="fixed top-0 z-50 flex h-12 w-full place-items-center justify-end space-x-1 px-2">
       <ThemeSwitch />
     </header>
   );
