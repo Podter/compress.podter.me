e24c8db6ca03645e0cad032b6d4094c58e8c427d
Nonthaphat Chaisu <48936015+Podter@users.noreply.github.com>
2024-07-31 23:38:10 +0700

    basic drag and drop

--- src/components/upload.tsx
+++ src/components/upload.tsx
@@ -1,10 +1,24 @@
 import { UploadIcon } from "lucide-react";
+import { useDropzone } from "react-dropzone";
 
 import { Button } from "~/components/ui/button";
+import { cn } from "~/lib/utils";
 
 export default function Upload() {
+  const { isDragAccept, getRootProps, getInputProps, open } = useDropzone({
+    noClick: true,
+    noKeyboard: true,
+  });
+
   return (
-    <div className="flex max-w-sm flex-col items-center space-y-6 rounded-lg bg-card p-8 text-card-foreground shadow-sm outline-dashed outline-2 outline-border">
+    <div
+      {...getRootProps({
+        className: cn(
+          "flex max-w-sm flex-col items-center space-y-6 rounded-lg bg-card p-8 text-card-foreground shadow-sm outline-dashed outline-2 outline-border",
+          isDragAccept && "bg-accent transition-colors dark:bg-accent/50",
+        ),
+      })}
+    >
       <UploadIcon size={48} />
       <div className="flex flex-col items-center space-y-2 text-center">
         <p>Drag and drop your video file to compress</p>
@@ -12,7 +26,8 @@ export default function Upload() {
           Everything happens in your browser, nothing is uploaded to the server.
         </p>
       </div>
-      <Button>Select file</Button>
+      <Button onClick={open}>Select file</Button>
+      <input {...getInputProps()} />
     </div>
   );
 }
