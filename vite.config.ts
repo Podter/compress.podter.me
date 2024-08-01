84fa7c684335e9a614ba52254eb4466178a019f0
Nonthaphat Chaisu <48936015+Podter@users.noreply.github.com>
2024-08-02 00:42:32 +0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -10,4 +10,13 @@ export default defineConfig({
       "~": path.resolve(__dirname, "./src"),
     },
   },
+  optimizeDeps: {
+    exclude: ["@ffmpeg/ffmpeg", "@ffmpeg/util"],
+  },
+  server: {
+    headers: {
+      "Cross-Origin-Opener-Policy": "same-origin",
+      "Cross-Origin-Embedder-Policy": "require-corp",
+    },
+  },
 });
