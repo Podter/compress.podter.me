19cd0a5512f4347be2145cecb9919ec235394efc
Nonthaphat Chaisu <48936015+Podter@users.noreply.github.com>
2024-08-02 00:05:43 +0700

    migrate to scss

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-import "./styles.css";
+import "./styles.scss";
 
 import App from "./app";
 import { FileProvider } from "./providers/file";
