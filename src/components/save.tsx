1054856da7a7d0cb1e58f00bb040d2e89bff11e0
Nonthaphat Chaisu <48936015+Podter@users.noreply.github.com>
2024-08-07 23:31:57 +0700

    Update save.tsx

--- src/components/save.tsx
+++ src/components/save.tsx
@@ -31,37 +31,35 @@ export default function Save({ originalFile, compressedFile }: SaveProps) {
         src={URL.createObjectURL(compressedFile)}
         controls
       />
-      <div className="mt-6 flex w-full flex-col items-center space-y-4">
-        <div className="flex flex-col space-y-3 text-center">
-          <p className="flex items-center justify-center space-x-1.5">
-            <Check size={16} />
-            <span>Video compressed successfully</span>
-          </p>
-          <div className="flex justify-center">
-            <div className="flex flex-col items-center space-y-1 rounded-xl border px-4 py-3 text-center text-sm shadow">
-              <div className="flex items-center space-x-2">
-                <p>{originalSize} MB</p>
-                <ArrowRight size={14} />
-                <p>{compressedSize} MB</p>
-              </div>
-              <p className="text-muted-foreground">Saved {savedPercentage}%</p>
+      <div className="mt-6 flex flex-col space-y-3 text-center">
+        <p className="flex items-center justify-center space-x-1.5">
+          <Check size={16} />
+          <span>Video compressed successfully</span>
+        </p>
+        <div className="flex justify-center">
+          <div className="flex flex-col items-center space-y-1 rounded-xl border px-4 py-3 text-center text-sm shadow">
+            <div className="flex items-center space-x-2">
+              <p>{originalSize} MB</p>
+              <ArrowRight size={14} />
+              <p>{compressedSize} MB</p>
             </div>
+            <p className="text-muted-foreground">Saved {savedPercentage}%</p>
           </div>
-          <p className="text-sm text-muted-foreground">
-            You can save the compressed video by clicking the save button below.
-          </p>
-          <div className="flex justify-center">
-            <Button asChild>
-              <a
-                href={URL.createObjectURL(compressedFile)}
-                download={"compressed-" + originalFile.name}
-                className="gap-2"
-              >
-                <span>Save video</span>
-                <Download size={16} />
-              </a>
-            </Button>
-          </div>
+        </div>
+        <p className="text-sm text-muted-foreground">
+          You can save the compressed video by clicking the save button below.
+        </p>
+        <div className="flex justify-center">
+          <Button asChild>
+            <a
+              href={URL.createObjectURL(compressedFile)}
+              download={"compressed-" + originalFile.name}
+              className="gap-2"
+            >
+              <span>Save video</span>
+              <Download size={16} />
+            </a>
+          </Button>
         </div>
       </div>
     </>
