5cc779c860a97a7c6470e815c082a2cf34080754
Nonthaphat Chaisu <48936015+Podter@users.noreply.github.com>
2024-08-10 22:25:56 +0700

    Update save.tsx

--- src/components/save.tsx
+++ src/components/save.tsx
@@ -58,6 +58,7 @@ export default function Save({ originalFile, compressedFile }: SaveProps) {
         ],
       });
     });
+  // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
   return (
