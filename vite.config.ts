702117068c1160e69d6f5ef4e3ae252f6a3d477b
Nonthaphat Chaisu <48936015+Podter@users.noreply.github.com>
2024-08-10 15:46:54 +0700

    basic pwa

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,37 @@
 import path from "path";
 import react from "@vitejs/plugin-react-swc";
 import { defineConfig } from "vite";
+import { VitePWA } from "vite-plugin-pwa";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: "prompt",
+      pwaAssets: {
+        disabled: false,
+        config: true,
+      },
+      manifest: {
+        name: "Video compressor",
+        short_name: "Video compressor",
+        description: "The simplest video compressor",
+        theme_color: "#ffffff",
+      },
+      workbox: {
+        globPatterns: ["**/*.{js,css,html,svg,png,ico}"],
+        cleanupOutdatedCaches: true,
+        clientsClaim: true,
+      },
+      devOptions: {
+        enabled: true,
+        navigateFallback: "index.html",
+        suppressWarnings: true,
+        type: "module",
+      },
+    }),
+  ],
   resolve: {
     alias: {
       "~": path.resolve(__dirname, "./src"),
